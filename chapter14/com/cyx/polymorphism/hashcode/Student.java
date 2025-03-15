7b6e413a24fdfe21a6845fdd2ccaff7ae4af3fb2
m13237097902 <m13237097902@gmail.com>
2025-03-15 08:18:26 +0000

    "完善 Student 类，重写 equals、hashCode 和 toString 方法，新增 finalize 方法，并更新 StudentTest 类以测试对象的相等性、哈希码和垃圾回收行为。"

--- chapter14/com/cyx/polymorphism/hashcode/Student.java
+++ chapter14/com/cyx/polymorphism/hashcode/Student.java
@@ -11,9 +11,33 @@ public class Student {
         this.age = age;
     }
 
+    //@Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        //比较类的定义是否一致
+        if(this.getClass() != o.getClass()) return false;
+        //类的定义一致，那个对象o就可以被强制转换为Student
+        Student other = (Student)o;
+        return this.name.equals(other.name) && this.age == other.age;
+
+    }
+
+
     //hashCode()方法被重写之后，返回的值就不再是对象的内存地址
     @Override
     public int hashCode(){
-        return 1;
+        return name.hashCode() + age;
+    }
+
+    @Override
+    public String toString(){
+        return name + "\t" + age;
+    }
+
+    //当一个Student对象变成垃圾事可能会被调用
+    @Override
+    protected void finalize() throws Throwable{
+        this.name = null;
+        System.out.println("所有资源已经释放完毕，可以开始清理了");
     }
 }
--- chapter14/com/cyx/polymorphism/hashcode/StudentTest.java
+++ chapter14/com/cyx/polymorphism/hashcode/StudentTest.java
@@ -1,9 +1,39 @@
+/*
+ * @Author: m13237097902 m13237097902@gmail.com
+ * @Date: 2025-03-12 16:19:34
+ * @LastEditors: m13237097902 m13237097902@gmail.com
+ * @LastEditTime: 2025-03-15 07:47:59
+ * @FilePath: /workspace/STUDY/chapter14/com/cyx/polymorphism/hashcode/StudentTest.java
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package chapter14.com.cyx.polymorphism.hashcode;
 
 public class StudentTest {
 
-    public static void main(String[] args) {
-        Student s1 = new Student("张三", 20);
-        Student s2 = new Student("张三", 20);
+    public static void main(String[] args) throws InterruptedException {
+        Student s1 = new Student("张三", 1);
+        Student s2 = new Student("张三", 1);
+        boolean result = s1.equals(s2);
+
+        System.out.println(result);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+
+        System.out.println(s1);
+
+        show();
+        //garbage collextor
+        System.gc();//调用系统的垃圾回收器进行垃圾回收
+        Thread.sleep(1000);
+        System.out.println("垃圾回收器开始工作，这是最后一行代码了");
+
     }
+
+    public static void show(){
+        //s对象的作用范围只是在show()方法中，一旦方法执行完毕，那么s对象就应该消亡，释放内存
+        Student s = new Student("张三", 1);
+        System.out.println(s);
+    }
+
+    
 }
